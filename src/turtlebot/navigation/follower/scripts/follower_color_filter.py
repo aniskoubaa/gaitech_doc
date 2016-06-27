2ca4e0a3969f9cb3814f7a853d0c47195faf78d7
Abdelrahman <a.mahmoud1994@gmail.com>
2016-06-27 13:42:15 +0900

    Adding the follower_line_finder.py file

--- src/turtlebot/navigation/follower/scripts/follower_color_filter.py
+++ src/turtlebot/navigation/follower/scripts/follower_color_filter.py
@@ -19,9 +19,11 @@ class Follower:
 	def image_callback(self, msg):
 		image = self.bridge.imgmsg_to_cv2(msg)
 		hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+		#create lower and upper boundaries for yellow color to produce the binary image
 		lower_yellow = numpy.array([ 50, 50, 170])
 		upper_yellow = numpy.array([255, 255, 190])
 		mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+		
 		masked = cv2.bitwise_and(image, image, mask=mask)
 		cv2.imshow("window", mask )
 		cv2.waitKey(3)
