26af229c4380a472d8a74de2116b64c8bfa168e1
Abdelrahman <a.mahmoud1994@gmail.com>
2016-06-27 14:01:22 +0900

    Adding documentation to  line_follower.py file

--- src/turtlebot/navigation/follower/scripts/line_follower.py
+++ src/turtlebot/navigation/follower/scripts/line_follower.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
 
+
+#This Program is tested on Gazebo Simulator
+#This script uses the cv_bridge package to convert images coming on the topic
+#sensor_msgs/Image to OpenCV messages and then convert their colors from RGB to HSV
+#then apply a threshold for hues near the color yellow to obtain the binary image 
+#to be able to see only the yellow line and then follow that line
+#It uses an approach called proportional and simply means 
+
+
+
 import rospy, cv2, cv_bridge, numpy
 from sensor_msgs.msg import Image
 from geometry_msgs.msg import Twist
@@ -38,6 +48,8 @@ class Follower:
 			cx = int(M['m10']/M['m00'])
 			cy = int(M['m01']/M['m00'])
 			cv2.circle(image, (cx, cy), 20, (0,0,255), -1)
+#The proportional controller is implemented in the following four lines	which
+#is reposible of linear scaling of an error to drive the control output.	
 			err = cx - w/2
 			self.twist.linear.x = 0.2
 			self.twist.angular.z = -float(err) / 100
